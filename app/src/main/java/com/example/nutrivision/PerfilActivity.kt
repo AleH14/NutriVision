6f7e2c37b5a06484626c596b2dc12415833bd63f
Daniel Ramirez <cesardanielguzmanramirez23@gmail.com>
2026-04-08 22:51:30 -0600

    feat: Creación de la interfaz del perfil
    Implementar la interfaz y modelo de perfil para incluir fecha de nacimiento, género, peso y altura siendo fiel al diseño solicitado.

--- app/src/main/java/com/example/nutrivision/PerfilActivity.kt
+++ app/src/main/java/com/example/nutrivision/PerfilActivity.kt
@@ -2,25 +2,22 @@ package com.example.nutrivision
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class PerfilActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
 
-        //menu barra de navegacion
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigation)
-// Marcar que estamos en inicio
         bottomNav.selectedItemId = R.id.nav_perfil
+
         bottomNav.setOnItemSelectedListener {
-
             when (it.itemId) {
-
                 R.id.nav_inicio -> {
                     startActivity(Intent(this, InicioActivity::class.java))
                     overridePendingTransition(0, 0)
@@ -39,6 +36,18 @@ class PerfilActivity : AppCompatActivity() {
 
                 else -> false
             }
+        }
+
+        findViewById<TextView>(R.id.btnBackPerfil).setOnClickListener {
+            finish()
+        }
+
+        findViewById<TextView>(R.id.btnEditarPerfil).setOnClickListener {
+            Toast.makeText(this, "Editar perfil", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<TextView>(R.id.btnCerrarSesion).setOnClickListener {
+            Toast.makeText(this, "Cerrar sesión", Toast.LENGTH_SHORT).show()
         }
     }
 }
